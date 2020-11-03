c4ef5a6a4c0250de6ab2e1b6738818ed3436991f
crystal  villanueva <noelani.villanueva@gmail.com>
2020-11-03 23:56:45 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -7,7 +7,7 @@ KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
-  cconstants=[{ name:  "Don Gately", occupation: "Live-in Staff" },
+  constants=[{ name:  "Don Gately", occupation: "Live-in Staff" },
     { name:  "Joelle van Dyne", occupation: "Radio Personality" },
     { name:  "Pat Monteseian", occupation: "Staff" },
     { name:  "Kate Gompert", occupation: "None" },
@@ -18,7 +18,7 @@ def assembled_aoh
 end
 
 def literal_aoh
-  
+  result = 
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
 end
